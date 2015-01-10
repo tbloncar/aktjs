012add735dfbc653939bc38a91a4dafcb237dfe6
Travis Loncar <loncar.travis@gmail.com>
2015-01-09 22:17:13 -0500

    Remove jQuery dependency.

--- README.md
+++ README.md
@@ -5,14 +5,14 @@ Akt is a small library that simplifies the task of passing data from views to
 event handlers. Its aim is to experiment with the concept of "event selectors"
 and to make data transfer (from HTML attributes to JS) intuitive. It makes use
 of Michael Best's JavaScript Object Literal Parser
-(https://github.com/mbest/js-object-literal-parse) and has a jQuery dependency.
+(https://github.com/mbest/js-object-literal-parse) and has no explicit
+dependencies.
 
 ### Usage
 
 Include the `akt.js` script in your project.
 
     ...
-    <script src="vendor/jquery.min.js"></script>
     <script src="js/akt.js"></script>
     ...
 
@@ -26,14 +26,10 @@ event handlers.
 
 Use Akt's `on` method to bind an event handler to a selector-event pair.
 
-    $(document).ready(function() {
-    
       Akt.on('the-btn', 'click', function(e, data) {
         alert("NAME: " + data.name); 
         alert("NUMBER: " + data.num); 
       });
-
-    });
 
 You have access to both the associated `event` and `data` within the callback
 function.
--- akt.js
+++ akt.js
@@ -12,7 +12,7 @@
 \_\___\     /____/_/\/_/      \_\_\\_\/ \/_/    \/_______/     \_____\/    
 
 
-akt.js | version 0.0.1
+akt.js | version 0.0.2
 (c) Travis Loncar (https://github.com/tbloncar)
 
 */
@@ -20,7 +20,7 @@ akt.js | version 0.0.1
 // javascript object literal parser (https://github.com/mbest/js-object-literal-parse)
 (function(e,t){if(typeof exports==="object"){module.exports=t()}else if(typeof define==="function"&&define.amd){define(t)}else{e.parseObjectLiteral=t()}})(this,function(e){function l(e){return e==null?"":e.trim?e.trim():e.toString().replace(/^[\s\xa0]+|[\s\xa0]+$/g,"")}var t='"(?:[^"\\\\]|\\\\.)*"',n="'(?:[^'\\\\]|\\\\.)*'",r="/(?:[^/\\\\]|\\\\.)*/w*",i=",\"'{}()/:[\\]",s="[^\\s:,/][^"+i+"]*[^\\s"+i+"]",o="[^\\s]",u=RegExp(t+"|"+n+"|"+r+"|"+s+"|"+o,"g"),a=/[\])A-Za-z0-9_$]$/,f=/(in|return|typeof)$/;return function(t){var n=l(t);if(n.charCodeAt(0)===123)n=n.slice(1,-1);var r=[],i=n.match(u),s,o,c=0;if(i){i.push(",");for(var h=0,p;p=i[h];++h){var d=p.charCodeAt(0);if(d===44){if(c<=0){if(s)r.push([s,o?o.join(""):e]);s=o=c=0;continue}}else if(d===58){if(!o)continue}else if(d===47&&h&&p.length>1){if(a.test(i[h-1])&&!f.test(i[h-1])){n=n.substr(n.indexOf(p)+1);i=n.match(u);i.push(",");h=-1;p="/"}}else if(d===40||d===123||d===91){++c}else if(d===41||d===125||d===93){--c}else if(!s&&!o){s=d===34||d===39?p.slice(1,-1):p;continue}if(o)o.push(p);else o=[p]}}return r}});
 
-var Akt = (function(jQ, pOL, undefined) {
+var Akt = (function(pOL, undefined) {
 
   // Parse akt data string
   function parseDataStr(str) {
@@ -33,41 +33,53 @@ var Akt = (function(jQ, pOL, undefined) {
     return data;
   }
 
+  // Wrap DOM nodes for decoration
+  function AktWrapper(nodes) {
+    this.nodes = nodes;
+
+
+    // Bind event listener (w/ extra data param) to each node
+    this.on = function(evnt, cb) {
+      var node, dataStr;
+
+      for(var i = 0, l = nodes.length; i < l; i++) {
+        node = nodes[i];
+        dataStr = node.getAttribute('data-akt-d');
+
+        node.addEventListener(evnt, function(e) {
+          cb.call(this, e, parseDataStr(dataStr)); 
+        });
+      } 
+    };
+  }
+
   return {
     
     /**
-     * Wraps elements that match AKT selector in a jQuery object.
+     * Returns matching nodes in AktWrapper.
      *
-     * @param {string} slctr - The AKT selector.
+     * @param {string} slctr - The Akt selector.
      */
-    $: function(slctr) {
-      return jQ('[data-akt-s="' + slctr + '"]'); 
+    get: function(slctr) {
+      var nodes = document.querySelectorAll('[data-akt-s="' + slctr + '"]');
+
+      return new AktWrapper(nodes);
     },
 
     /**
-     * Binds handler to elements that match AKT selector.
+     * Binds handler to elements that match Akt selector.
      *
-     * @param {string} slctr - The AKT selector.
+     * @param {string} slctr - The Akt selector.
      * @param {string} evnt - The DOM event name.
      * @param {function} cb - The event handler function.
      */
     on: function(slctr, evnt, cb) {
-      this.$(slctr).on(evnt, function(e) {
-        var dataStr = $(this).data('aktD');
+      this.get(slctr).on(evnt, function(e) {
+        var dataStr = this.getAttribute('data-akt-d');
 
         cb.call(this, e, parseDataStr(dataStr)); 
       }); 
-    },
-
-    /**
-     * Unbinds all handlers for event from elements that match AKT selector.
-     *
-     * @param {string} slctr - The AKT selector.
-     * @param {string} evnt - The DOM event name.
-     */
-    off: function(slctr, evnt) {
-      this.$(slctr).off(evnt);
     }
   };
 
-}(jQuery, parseObjectLiteral));
+}(parseObjectLiteral));
